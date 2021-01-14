8770f61210b2e2181bd00d52e4d4d6d095d07b3f
Yura Ko <dsaqaz421@naver.com>
2021-01-14 12:30:57 +0900

    0.4 First Steps

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Nicolas", age = 24, gender = "Male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, your are ${age}, you are a ${gender}`);
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "Nicolas",
+    age = 24,
+    gender = "Male";
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, your are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
