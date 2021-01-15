4e8b7a05286e9c9d9ed83fa327293d26848b21cb
Yura Ko <dsaqaz421@naver.com>
2021-01-15 12:31:42 +0900

    0.6 Interfaces on Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const name = "Nicolas", age = 24, gender = "Male";
-// setting require type
-const sayHi = (name, age, gender) => {
-    //console.log(`Hello ${name}, your are ${age}, you are a ${gender}`);
-    return `Hello ${name}, your are ${age}, you are a ${gender}`;
+const person = {
+    name: "Temp",
+    age: 22,
+    gender: "male"
 };
-console.log(sayHi(name, age, gender));
-console.log(sayHi("Nicolas", 444, "male"));
+// setting require type
+//const sayHi = (name:string, age:number, gender:string):string => {            // setting return type
+const sayHi = (person) => {
+    //console.log(`Hello ${name}, your are ${age}, you are a ${gender}`);
+    return `Hello ${person.name}, your are ${person.age}, you are a ${person.gender}`;
+};
+//console.log(sayHi(name, age, gender));          // return const val
+//console.log(sayHi("Nicolas", 444, "male"));     // return string val
+console.log(sayHi(person)); // return object
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
+// interface는 오직 ts에서만 가능
+interface Human{
+    name : string,
+    age : number,
+    gender : string
+}
+
 const name = "Nicolas",
     age = 24,
     gender = "Male";
 
+const person = {
+    name : "Temp",
+    age : 22,
+    gender : "male"    
+}
+
 // setting require type
-const sayHi = (name:string, age:number, gender:string):string => {            // setting return type
+//const sayHi = (name:string, age:number, gender:string):string => {            // setting return type
+const sayHi = (person : Human):string => {            // setting return type is object
     //console.log(`Hello ${name}, your are ${age}, you are a ${gender}`);
-    return `Hello ${name}, your are ${age}, you are a ${gender}`;
+    return `Hello ${person.name}, your are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(name, age, gender));
-console.log(sayHi("Nicolas", 444, "male"));
+//console.log(sayHi(name, age, gender));          // return const val
+//console.log(sayHi("Nicolas", 444, "male"));     // return string val
+console.log(sayHi(person))                    // return object
 
 export {};
